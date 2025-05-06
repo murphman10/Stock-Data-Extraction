c4a93c068bb8c80be0e6e66097c8f1e544392d75
Asa Murphy <asa.g.murphy75@gmail.com>
2025-05-06 14:30:55 -0400

    Update Extract_n_Visualize_StockData.py

--- Extract_n_Visualize_StockData.py
+++ Extract_n_Visualize_StockData.py
@@ -18,12 +18,54 @@ import warnings
 # Ignore all warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+#Use yf to extract stock data for Tesla and GameStop
+tesla = yf.Ticker("TSLA")
+gamestop = yf.Ticker("GME")
+
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+
+gme_data = gamestop.history(period="max")
+gme_data.reset_index(inplace=True)
+
+#Use webscraping to get revenue data from Tesla and GameStop
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, 'html.parser')
+
+url_gme = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+html_data_2 = requests.get(url_gme).text
+soup_gme = BeautifulSoup(html_data_2, 'html.parser')
+
+tesla_revenue = pd.DataFrame()
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text
+    tesla_revenue = pd.concat([tesla_revenue, pd.DataFrame({"Date":[date], "Revenue":[revenue]})], ignore_index=True)
+
+#tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+#tesla_revenue.dropna(inplace=True)
+#tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+#tesla_revenue.tail()
+
+gme_revenue = pd.DataFrame()
+for row in soup_gme.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text
+    gme_revenue = pd.concat([gme_revenue, pd.DataFrame({"Date":[date], "Revenue":[revenue]})], ignore_index=True)
+
+#Plot Tesla and GameStop data on a graph
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+make_graph(gme_data, gme_revenue, 'GameStop')
+
 def make_graph(stock_data, revenue_data, stock):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
     stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
     revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
     fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("string"), name="Revenue"), row=2, col=1) #Revenue.astype was "float"
     fig.update_xaxes(title_text="Date", row=1, col=1)
     fig.update_xaxes(title_text="Date", row=2, col=1)
     fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
